bab003fd7c91bf02223cb93e576f8956b623b42e
Yotam Tal <yotam978@gmail.com>
2019-06-12 00:57:36 +0300

    add open api route

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 const http          = require('http');
-const path          = require('path');
-const url           = require('url');
+const Dispatcher    = require('httpdispatcher');
 const fs            = require('fs');
 const spawn         = require('child_process').spawn;
 const socketIO      = require('socket.io');
@@ -9,6 +8,7 @@ const middleware    = require('socketio-wildcard')();
 const kermit        = spawn('kermit' ,[__dirname + '/opendoor.sh'] );
 const port          = process.argv[2] || 3333;
 
+let dispatcher      = new Dispatcher();
 let opening         = false;
 
 kermit.stderr.on('data', (data) => console.log(`ps stderr: ${data}`) );
@@ -34,18 +34,95 @@ kermit.stdout.on('data', (data) => {
     }
 });
 
-let server_config = (req, res) => {
-    res.statusCode = 400;
-    res.end(`Nothing to do here.`);
+let open_door = (uuid, from_home) => {
+    /***
+    * Get allowed devices UUID from stored JSON
+    */
+    try {
+        let obj = JSON.parse(fs.readFileSync(__dirname + '/devices.json', 'utf-8'))
+        /***
+         * If device connected from home
+         * and not listed in allowed JSON
+         * add the UUID to the whitelist
+         */
+        if(from_home && obj.allowed.indexOf(uuid) === -1){
+            obj.allowed.push(uuid)
+            json = JSON.stringify(obj)
+            fs.writeFile(__dirname + '/devices.json', json, 'utf8', (e) => e === null && console.log('Added a new device'))
+            return 'added'
+        }
+
+        /***
+         * Device is whitelisted - Try opening the door
+         */
+        if(obj.allowed.indexOf(uuid) !== -1){
+            /***
+             * Abort if someone already opened 
+             * in the last 10 secondes
+             */
+            if(opening) return 'busy'
+
+            /***
+             * All clear - Open!
+             */
+            else {
+                console.log(new Date() + ': Opening the door');
+
+                opening = true;
+
+                kermit.stdin.write('ATH1\r\n')
+                kermit.stdin.write('ATDP551\r\n')
+                
+                setTimeout( () => opening = false, 10000)
+
+                return 'opening'
+            }
+
+        }
+        /***
+         * Device is not whitelisted 
+         * and not local connection]
+         * Deny request
+         */
+        else return 'denied' 
+            
+    } catch (err) {
+        console.log(err)
+        console.log('JSON read error!')
+        return 'error' 
+    }
+    
+}
+
+let server_config = (request, response) => {
+    try {
+        // log the request on console
+        console.log(request.url)
+        // Dispatch
+        dispatcher.dispatch(request, response)
+    } catch(err) {
+        console.log(err)
+    }
 }
 
 app = http.createServer(server_config);
-io = socketIO(app);
 
-io.on('error', (e) => console.log(e) );
-io.use(middleware);
+dispatcher.onPost("/open", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    let payload = JSON.parse(req.body);
+    let open_res = 'denied';
+    if(payload && payload.uuid)
+        open_res = open_door(payload.uuid)
+    
+    res.end(JSON.stringify({"res":open_res}));
+});
+
+
+io = socketIO(app)
+io.use(middleware)
+
+io.on('error', (e) => console.log(e) )
 io.on('connection', (socket) => {
-
     /**
      * Get connected client IP adress
      */
@@ -54,80 +131,36 @@ io.on('connection', (socket) => {
     // console.log(clientIPV4 + ' connected');
     // socket.on('disconnect', () => console.log(clientIPV4 + ' disconnected') );
 
-    socket.on('*', function (data) {
-        let uuid = data.data[1].uuid;
-        let action = data.data[0];
+    socket.on('*', function (payload) {
+        
+        let uuid = payload.data[1].uuid,
+            action = payload.data[0]
 
         /***
          * If ip starts with a local ip address numbers
          * Allow device to be added to stored JSON
          */
-        let fromHome = false;
-        let reqIp = clientIPV4.split('.');
-        if(reqIp[0] == "10" && reqIp[1] == "100" && reqIp[2] == '102') 
-            fromHome = true;
+        let from_home = false,
+            req_ip = clientIPV4.split('.');
+
+        if(req_ip[0] == "10" && req_ip[1] == "100" && req_ip[2] == '102') 
+            from_home = true;
 
         /**
          * If action requested is 'open' and uuid is set...
          */
         if(action === 'open' && uuid){
-            /***
-             * Get allowed devices UUID from stored JSON
-             */
-            try {
-                let obj = JSON.parse(fs.readFileSync(__dirname + '/devices.json', 'utf-8'))
-                /***
-                 * If device connected from home
-                 * and not listed in allowed JSON
-                 * add the UUID to the whitelist
-                 */
-                if(fromHome && obj.allowed.indexOf(uuid) === -1){
-                    obj.allowed.push(uuid);
-                    json = JSON.stringify(obj);
-                    socket.emit('added', { uuid: uuid })
-                    fs.writeFile(__dirname + '/devices.json', json, 'utf8', (e) => e === null && console.log('Added a new device')); 
-                }
-
-                /***
-                 * Device is whitelisted - Try opening the door
-                 */
-                if(obj.allowed.indexOf(uuid) !== -1){
-                    /***
-                     * Abort if someone already opened 
-                     * in the last 10 secondes
-                     */
-                    if(opening) socket.emit('busy', { uuid: uuid });
-
-                    /***
-                     * All clear - Open!
-                     */
-                    else {
-                        console.log(new Date() + ': Opening the door');
-
-                        opening = true;
-                        socket.broadcast.emit('opening', { uuid: uuid })
-
-                        kermit.stdin.write('ATH1\r\n');
-                        kermit.stdin.write('ATDP551\r\n');
-
-                        setTimeout( () => opening = false, 10000);
-                    }
-
-                }
-                /***
-                 * Device is not whitelisted 
-                 * and not local connection]
-                 * Deny request
-                 */
-                else socket.emit('denied', { uuid: uuid })
-
-            } catch (err) {
-                console.log(err)
-                console.log('JSON read error!');
-                socket.emit('denied', { uuid: uuid })
+            let open_res = open_door(uuid, from_home);
+            switch(open_res){
+                case 'opening':
+                    socket.broadcast.emit('opening', { uuid: uuid })
+                    break;
+                default:
+                    socket.emit(open_res, { uuid: uuid })
+                    break;
             }
-                
-        } 
+        }
+        
         /***
          * Unkown action
          */
