68c83cb541660a78d3d6d6157d1b66c7dcb4c9d7
Yotam Tal <yotam978@gmail.com>
2019-06-11 23:41:45 +0300

    Clean up

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-const app           = require('http').createServer(handler);
+const app           = require('http').createServer(server_config);
 const path          = require('path');
 const url           = require('url');
 const fs            = require('fs');
@@ -9,12 +9,13 @@ const middleware    = require('socketio-wildcard')();
 const kermit        = spawn('kermit' ,[__dirname + '/opendoor.sh'] );
 const port          = process.argv[2] || 3333;
 
-
 let opening         = false;
 
+kermit.stderr.on('data', (data) => console.log(`ps stderr: ${data}`) );
+kermit.on('close', (code) => code !== 0 && console.log(`ps process exited with code ${code}`) );
 kermit.stdout.on('data', (data) => {
     let msegSplit = data.toString().split('\n');
-    console.log(data.toString());
+    // console.log(data.toString());
     if( msegSplit[0].startsWith('?OpenSSL') ) 
         kermit.stdin.write('AT+VDR=1,4\r\n');
     if( msegSplit[0].startsWith('BUSY') ) 
@@ -24,7 +25,7 @@ kermit.stdout.on('data', (data) => {
     // if( msegSplit.length >= 2 && msegSplit[2].startsWith('RING') ) 
         // console.log('Incoming landline call');
     if( msegSplit.length >= 5 && msegSplit[6].startsWith('RING') ){
-        console.log('Doorphone');
+        // console.log('Doorphone');
         ringing = true;
         io.emit('ringing', {});
         setTimeout(function(){ 
@@ -33,111 +34,104 @@ kermit.stdout.on('data', (data) => {
     }
 });
 
-kermit.stderr.on('data', (data) => {
-    console.log(`ps stderr: ${data}`);
-});
+io.on('error', (e) => console.log(e) );
 
-kermit.on('close', (code) => {
-    if (code !== 0) {
-        console.log(`ps process exited with code ${code}`);
-    }
-});
-
-io.on('error', function(e){	
-	console.log(e)
-});
-
-function handler (req, res) {
-    // parse URL
-    const parsedUrl = url.parse(req.url);
-    // extract URL path serve index.html by default
-    let pathname = `.${parsedUrl.pathname}`;
-    if(pathname === './') 
-        pathname = `./index.html`;
-    // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-    const ext = path.parse(pathname).ext;
-    // maps file extention to MIME typere
-    const map = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css'
-    };
-
-    fs.exists(pathname, function (exist) {
-        if(!exist) {
-            // if the file is not found, return 404
-            res.statusCode = 404;
-            res.end(`File ${pathname} not found!`);
-            return;
-        }
-
-        // if is a directory search for index file matching the extention
-        if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
-
-        // read file from file system
-        fs.readFile(pathname, function(err, data){
-            if(err){
-            res.statusCode = 500;
-            res.end(`Error getting the file: ${err}.`);
-            } else {
-            // if the file is found, set Content-type and send data
-            res.setHeader('Content-type', map[ext] || 'text/plain' );
-            res.end(data);
-            }
-        });
-    });
+let server_config = (req, res) => {
+    res.statusCode = 400;
+    res.end(`Nothing to do here.`);
 }
 
 io.use(middleware);
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
+
+    /**
+     * Get connected client IP adress
+     */
     let clientIPV4 = socket.handshake.address.split('::ffff:')[1];
-    console.log(clientIPV4 + ' connected');
-    socket.on('disconnect', function() {
-        console.log(clientIPV4 + ' disconnected');
-    });
+    
+    // console.log(clientIPV4 + ' connected');
+    // socket.on('disconnect', () => console.log(clientIPV4 + ' disconnected') );
+
     socket.on('*', function (data) {
         let uuid = data.data[1].uuid;
         let action = data.data[0];
+
+        /***
+         * If ip starts with a local ip address numbers
+         * Allow device to be added to stored JSON
+         */
         let fromHome = false;
         let reqIp = clientIPV4.split('.');
-        if(reqIp[0] == "10" && reqIp[1] == "100" && reqIp[2] == '102')
+        if(reqIp[0] == "10" && reqIp[1] == "100" && reqIp[2] == '102') 
             fromHome = true;
 
+        /**
+         * If action requested is 'open' and uuid is set...
+         */
         if(action === 'open' && uuid){
-            fs.readFile(__dirname + '/devices.json', 'utf8', function readFileCallback(err, file){
-                if (err)
-                    console.log(err);
-                else {
-                obj = JSON.parse(file); 
+            /***
+             * Get allowed devices UUID from stored JSON
+             */
+            try {
+                let obj = JSON.parse(fs.readFileSync('/devices.json', 'utf-8'))
+                /***
+                 * If device connected from home
+                 * and not listed in allowed JSON
+                 * add the UUID to the whitelist
+                 */
                 if(fromHome && obj.allowed.indexOf(uuid) === -1){
                     obj.allowed.push(uuid);
                     json = JSON.stringify(obj);
                     socket.emit('added', { uuid: uuid })
                     fs.writeFile(__dirname + '/devices.json', json, 'utf8', (e) => e === null && console.log('Added a new device')); 
                 }
+
+                /***
+                 * Device is whitelisted - Try opening the door
+                 */
                 if(obj.allowed.indexOf(uuid) !== -1){
-                    if(opening === false){
+                    /***
+                     * Abort if someone already opened 
+                     * in the last 10 secondes
+                     */
+                    if(opening) socket.emit('busy', { uuid: uuid });
+
+                    /***
+                     * All clear - Open!
+                     */
+                    else {
                         console.log(new Date() + ': Opening the door');
+
                         opening = true;
-                        // console.log(opening);
                         socket.broadcast.emit('opening', { uuid: uuid })
+
                         kermit.stdin.write('ATH1\r\n');
                         kermit.stdin.write('ATDP551\r\n');
-                        setTimeout(function(){ 
-                            opening = false;
-                            // console.log(opening);
-                        }, 10000);
-                    } else {
-                        console.log('busy');
-                        socket.emit('busy', { uuid: uuid });
+
+                        setTimeout( () => opening = false, 10000);
                     }
-                } else {
-                    socket.emit('denied', { uuid: uuid })
+
                 }
-            }});
-        } else socket.emit('denied', { uuid: uuid })
+                /***
+                 * Device is not whitelisted 
+                 * and not local connection]
+                 * Deny request
+                 */
+                else socket.emit('denied', { uuid: uuid })
+
+            } catch (err) {
+                console.log('JSON read error!');
+            }
+                
+        } 
+        /***
+         * Unkown action
+         */
+        else socket.emit('denied', { uuid: uuid });
+
     });
-    socket.on('disconnect', function() {
+
+    socket.on('disconnect', () => {
         if(Object.keys(io.sockets.sockets).length === 0 ) 
             broadcast = null;
      });
